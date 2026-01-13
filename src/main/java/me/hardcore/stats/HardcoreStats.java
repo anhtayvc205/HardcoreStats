d05dc3c779a9fee271376d9242344fa45185dd51
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-13 15:19:20 +0700

    Update HardcoreStats.java

--- src/main/java/me/hardcore/stats/HardcoreStats.java
+++ src/main/java/me/hardcore/stats/HardcoreStats.java
@@ -1,18 +1,13 @@
 package me.hardcore.stats;
 
-import github.scarsz.discordsrv.DiscordSRV;
-import net.dv8tion.jda.api.JDA;
+import github.scarsz.discordsrv.api.DiscordUtil;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HardcoreStats extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        saveDefaultConfig();
-
-        JDA jda = DiscordSRV.api.getMainGuild().getJda();
-        DiscordSlashStats.register(jda);
-
-        getLogger().info("HardcoreStats enabled (Discord Slash)");
+        DiscordUtil.getApi().subscribe(new DiscordStatsListener());
+        getLogger().info("HardcoreStats hooked into DiscordSRV");
     }
 }
